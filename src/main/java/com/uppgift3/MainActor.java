46c14948d7e90324d0f2395b7d280bed5351ad08
martinB <martin.bjersten@gmail.com>
2021-04-26 13:13:27 +0200

    Started working on uppgift 3

---
+++ src/main/java/com/uppgift3/MainActor.java
@@ -0,0 +1,24 @@
+package com.uppgift3;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
+public class MainActor {
+    public static void main(String[] args) {
+        ActorSystem actorSystem = ActorSystem.create("MultitaskerSystem");
+        int noActors = 500;
+        ActorRef[] actors = new ActorRef[noActors];
+
+        for (int i = 0; i < noActors; i++) {
+            actors[i] = actorSystem.actorOf(Props.create(MultitaskerActor.class));
+            actors[i].tell(i, ActorRef.noSender());
+        }
+        for (int i = 0; i < noActors; i++) {
+            actors[i].tell("work", ActorRef.noSender());
+        }
+        System.out.println("main thread done");
+    }
+
+
+}
