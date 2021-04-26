064f619d3b05662720af1f6c379b96b7e518bcb4
martinB <martin.bjersten@gmail.com>
2021-04-26 14:07:02 +0200

    Started working on uppgift 3

--- src/main/java/com/uppgift3/Main.java
+++ src/main/java/com/uppgift3/Main.java
@@ -4,7 +4,7 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 
-public class MainActor {
+public class Main {
     public static void main(String[] args) {
         ActorSystem actorSystem = ActorSystem.create("MultitaskerSystem");
         int noActors = 500;
