4e7b921454fced747ad617851894043479e66f2a
martinB <martin.bjersten@gmail.com>
2021-05-18 14:01:05 +0200

    Försökt få till test 4

---
+++ src/main/java/com/Test4/Account.java
@@ -0,0 +1,97 @@
+package com.Test4;
+
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.AbstractBehavior;
+import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.actor.typed.javadsl.Receive;
+import com.uppgift4.BankAccount;
+import com.uppgift4.MainActor4;
+
+public class Account extends AbstractBehavior<Account.Command> {
+
+    //actor variables
+    //this bank accounts unique id
+
+    private int id;
+    private int balance = 0;
+
+
+    interface Command{}
+
+    /////////////////////////////////////////// Commands we can receive //////////////////////////////////////////////////////////
+
+
+    public static class GetMoneyFromAccount implements Command {
+
+        public GetMoneyFromAccount(){
+
+        }
+    }
+
+    public static class DepositMoneyToAccount implements Command {
+        private final int money;
+        public DepositMoneyToAccount(int money){
+            this.money = money;
+        }
+    }
+
+    public static class WithdrawMoneyFromAccount implements Command {
+        private final int money;
+        public WithdrawMoneyFromAccount(int money){
+            this.money = money;
+        }
+    }
+
+    /////////////////////////////////////////// Handle the received message //////////////////////////////////////////////////////////
+
+    @Override
+    public Receive<Command> createReceive() {
+        return newReceiveBuilder()
+                .onMessage(GetMoneyFromAccount.class, this::onGetMoneyFromAccount)
+                .onMessage(DepositMoneyToAccount.class, this::onDepositMoneyToAccount)
+                .onMessage(WithdrawMoneyFromAccount.class, this::onWithdrawMoneyFromAccount)
+                .build();
+    }
+
+    /**
+     * Constructor
+     * @param context
+     */
+    private Account(ActorContext<Command> context, int id) {
+        super(context);
+        this.id = id;
+    }
+
+
+    /**
+     * Factory method
+     * @return
+     */
+    public static Behavior<Command> create(int id) {
+
+        return Behaviors.setup(context -> new Account(context, id));
+    }
+
+
+    /////////////////////////////////////////// Do things after a message has been received //////////////////////////////////////////////////////////
+
+
+
+    private Behavior<Command> onGetMoneyFromAccount(GetMoneyFromAccount getMoneyFromAccount){
+        System.out.println("Account " + id + " has " + balance + " money");
+        return this;
+    }
+
+    private Behavior<Command> onDepositMoneyToAccount(DepositMoneyToAccount depositMoneyToAccount){
+        balance = balance + depositMoneyToAccount.money;
+        return this;
+    }
+
+    private Behavior<Command> onWithdrawMoneyFromAccount(WithdrawMoneyFromAccount withdrawMoneyFromAccount){
+        balance = balance - withdrawMoneyFromAccount.money;
+        return this;
+    }
+
+}
---
+++ src/main/java/com/Test4/Main4.java
@@ -0,0 +1,23 @@
+package com.Test4;
+
+import akka.actor.typed.ActorSystem;
+import com.typesafe.config.ConfigFactory;
+
+public class Main4 {
+    public static void main(String[] args) {
+        long timeBeforeSetup = System.nanoTime();
+        int workers = 50;
+        int accounts = 50;
+        int loops = 1000;
+        ConfigFactory.load("src/main/resources/application.conf");
+        //#actor-system
+        ActorSystem<Bank.Command> mainActor = ActorSystem.create(Bank.create(), "mainActorSystem");
+        //#actor-system
+
+        //#main-send-messages
+        mainActor.tell(new Bank.SetNumberOfWorkersAndAccountsAndLoops(accounts, loops,workers, timeBeforeSetup));
+        //tell it to start
+        mainActor.tell(Bank.Start.INSTANCE);
+        //#main-send-messages
+    }
+}
